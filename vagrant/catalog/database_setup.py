26fb4c4d010c4c51093a57f204141ed6c1ce1a75
Trevor Thomas <trevorthomas@mac.com>
2018-05-28 14:06:43 -0400

    Setup database_setup.py

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -2,3 +2,54 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+class CategoryItem(Base):
+    __tablename__ = 'categoryitem'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    description = Column(String(500))
+    # establishes that Category Items must be a part of items
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    # differentiate between users that created items and those that have not.
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category': self.category,
+        }
+
+engine = create_engine('sqlite:///itemcatalog.db')
+
+Base.metadata.create_all(engine)
